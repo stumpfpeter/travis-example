4329cfc3201c3f87bffc61d2d54315e199e402d4
tozogabee <tozogabee@gmail.com>
2017-09-12 21:03:00 +0200

    5th commit

--- src/test/java/com/stumpf/travisexample/AddNumbersTest.groovy
+++ src/test/java/com/stumpf/travisexample/AddNumbersTest.groovy
@@ -1,12 +1,10 @@
 package com.stumpf.travisexample
 
-import org.junit.Test
-
 public class AddNumbersTest{
 
     @Test
     public void AddThreeNumbersTest() {
-        assertThat(new AddNumbers().addThreeNumbers(2,3,5).is(10));
+        assertThat(new AddNumbers().addThreeNumbers(2,3,5), is(10));
 
     }
 }
